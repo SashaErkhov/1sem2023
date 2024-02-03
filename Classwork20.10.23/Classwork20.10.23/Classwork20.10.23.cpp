3e961cc326157dea9fade0a4893c20cb4148bc4d
Sasha Erkhov <146490133+SashaErkhov@users.noreply.github.com>
2024-02-03 20:27:52 +0300

    Add files via upload

---
+++ Classwork20.10.23/Classwork20.10.23/Classwork20.10.23.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <fstream>//Файловые потоки
+
+
+int main()
+{
+    std::ifstream inFile("readme.txt");//Файлик на чтение 
+    if (!inFile)
+    {
+        std::cout << "Xui";
+        return 1;
+    }
+    std::ofstream outFile("output.txt", std::ios_base::binary | std::ios_base::app);//Файлик на запись
+    //update( | std::ios_base::app ): Дописывание одновременно с binary
+    if (!outFile)
+    {
+        std::cout << "Xui";
+        inFile.close();
+        return 2;
+    }
+    int ch;// Это лучше чем char ch;
+    int count = 0;
+    while ((ch = inFile.get()) != EOF)
+    {
+        outFile.put(ch);
+        ++count;
+    }
+    std::cout << "Total: " << count << " bytes" << std::endl;
+    inFile.close();
+    outFile.close();
+    return 0;
+}
---
+++ Classwork20.11.23/Classwork20.11.23/Classwork20.11.23.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+#include <fstream>
+
+bool generate_file(int n, const char* fname)
+{
+    std::ofstream outfile;
+    outfile.open(fname);
+    if (!outfile) return false;
+    while (n-- > 0)
+    {
+        outfile << " " << rand();
+    }
+    outfile.close();
+    return true;
+}
+
+bool _split_file(const char* fname, const char* tmp1, const char* tmp2)
+{
+    std::ifstream inFile(fname);
+    if (inFile) throw "can not open...";
+    std::ofstream outFile[2];
+    outFile[0].open(tmp1);
+    if (!outFile[0])
+    {
+        inFile.close();
+        throw "Can not open...";
+    }
+    outFile[0].open(tmp1);
+    if (!outFile[1])
+    {
+        inFile.close();
+        outFile[0].close();
+        throw "Can not open...";
+    }
+    bool answer = false;
+
+}
+
+int main()
+{
+    const char fname[] = "data.txt";
+    int N = 1000;
+    generate_file(N, fname);
+    return 0;
+}
